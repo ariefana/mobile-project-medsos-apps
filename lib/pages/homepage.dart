5356ba7901d4b51bcda6da3634005097914ef714
ariefana <ariefmudasir79@gmail.com>
2025-10-22 17:04:41 +0700

    mengubah text di halaman home

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -48,7 +48,7 @@ class HomePage extends StatelessWidget {
     
       body: Center(
         child: Text(
-          'This Home Page',
+          'This Home Page you should coding here',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
